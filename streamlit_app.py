13fd0cc4aa2a2f801e59799cc3a1c2e77bade5c2
Nehal <149889556+nehal404@users.noreply.github.com>
2024-04-29 13:48:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,40 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import ollama
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title("Ollama Python Chatbot")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# initialize history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# init models
+if "model" not in st.session_state:
+    st.session_state["model"] = ""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+models = [model["name"] for model in ollama.list()["models"]]
+st.session_state["model"] = st.selectbox("Choose your model", models)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+def model_res_generator():
+    stream = ollama.chat(
+        model=st.session_state["model"],
+        messages=st.session_state["messages"],
+        stream=True,
+    )
+    for chunk in stream:
+        yield chunk["message"]["content"]
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Display chat messages from history on app rerun
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+if prompt := st.chat_input("What is up?"):
+    # add latest message to history in format {role, content}
+    st.session_state["messages"].append({"role": "user", "content": prompt})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message = st.write_stream(model_res_generator())
+        st.session_state["messages"].append({"role": "assistant", "content": message})
